00e9c00b51a268e40564781b9db494fb65b210a7
RotCool <Rotten_LKZ@163.com>
2026-03-17 18:29:48 +0800

    GitHub Actions & About me page content

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
-export const SITE_TITLE = 'Rotcool 的个人博客';
-export const SITE_DESCRIPTION = '欢迎来到我的二次元风格博客~';
+export const SITE_TITLE = 'RotCool 小窝';
+export const SITE_DESCRIPTION = '神秘小地喵~';
 
 export const SITE_OWNER = {
-	name: 'Rotcool',
+	name: 'RotCool',
 	avatar: 'https://img.rotcool.me/i/2022/12/27/63aaca2a50b7d.jpg',
 	bio: '励志摆烂，想要创造有趣的事物',
 	social: {
